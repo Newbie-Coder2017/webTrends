d333be5feba56bda4253972db3ada0d16f4b56d8
Paul O <info@paulooidesign.com>
2018-05-10 15:48:51 -0400

    using the Header template PHP file for the header on about page

---
+++ about.php
@@ -0,0 +1,13 @@
+<?php
+$title = 'About us';
+$heading1 = 'Get to know us';
+require_once 'header.php';
+?>
+    <main>
+        <p>This is a description for the About page</p>
+    </main>
+    <footer>
+        &copy; Lab 1
+    </footer>
+</body>
+</html>
